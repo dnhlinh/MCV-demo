960ec974d735cffc2fe503ec2777730885889bfc
dnhlinh <49845413+dnhlinh@users.noreply.github.com>
2019-06-19 09:53:21 -0700

    Update clientsController.js

--- controllers/clientsController.js
+++ controllers/clientsController.js
@@ -8,7 +8,7 @@ const clients = require('./models/client')
 router.get('/', function(req,res) {
 	clients.find().sort('lastname').select({__v: 0, id: 0})
 	 .then(result => {
-	 	res.status(200).json({ressult: clients})
+	 	res.status(200).json({result: clients})
 	 })
 	 .catch(err => {
 	 	res.status(500).json({message: 'Something went wrong'})

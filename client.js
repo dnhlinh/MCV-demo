b0e7a016d954f42a60b240ab1daf8e621fd35ac0
dnhlinh <49845413+dnhlinh@users.noreply.github.com>
2019-06-19 09:41:03 -0700

    Add files via upload

---
+++ client.js
@@ -0,0 +1,10 @@
+const clients = [
+	{ firstname: "Linh", lastname: "Do", id: 1},
+	{ firstname: "Peter", lastname: "Pan", id: 2}
+];
+
+function getClient() {
+	return clients
+}
+
+console.log(getClient())

9ebc1aa469b6853a5c289229fd057c0042d56781
dnhlinh <49845413+dnhlinh@users.noreply.github.com>
2019-06-19 10:23:41 -0700

    Update client.js

--- models/client.js
+++ models/client.js
@@ -1,10 +1,46 @@
 const clients = [
-	{ firstname: "Linh", lastname: "Do", id: 1},
-	{ firstname: "Peter", lastname: "Pan", id: 2}
+	{ 
+	firstname: "Linh", 
+	lastname: "Do", 
+	id: 1
+	},
+	
+	{
+	 firstname: "Peter", 
+	 lastname: "Pan", 
+	 id: 2
+	}
 ];
 
 function getClient() {
 	return clients;
 }
 
+function find(key) {
+	for key in clients {
+		return key + ": " + clients[key];
+	}
+
+}
+
+function findone(idvalue) {
+	for id in clients {
+		if clients[id] = idvalue {
+			return clients[firstname, lastname]
+		}
+	}
+}
+
+function updateone(idvalue, firstnameUpdate, lastnameUpdate) {	
+
+}
+
+function deleteone(idvalue, firstnameUpdate, lastnameUpdate) {	
+
+}
+
 module.exports.getClient = getClient
+module.exports.find = find
+module.export.findone = findone
+module.export.updateone = updateone
+module.export.deleteone = deleteone
